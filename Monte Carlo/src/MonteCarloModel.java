feff7262344bcc0ae03a724a5424204ad7748a0a
YoussefSalah <96284359+YoussefSalah8@users.noreply.github.com>
2023-12-18 06:57:14 +0200

    Update MonteCarloModel.java

--- Monte Carlo/src/MonteCarloModel.java
+++ Monte Carlo/src/MonteCarloModel.java
@@ -12,10 +12,9 @@ public class MonteCarloModel implements Callable<Integer> {
         int pointsInsideCircle = 0;
 
         for (int i = 0; i < points; i++) {
-            double x = Math.random() * 2 - 1; // Random x coordinate between -1 and 1
-            double y = Math.random() * 2 - 1; // Random y coordinate between -1 and 1
+            double x = Math.random() * 2 - 1; 
+            double y = Math.random() * 2 - 1; 
 
-            // Check if the point is inside the unit circle
             if (x * x + y * y <= 1) {
                 pointsInsideCircle++;
             }
